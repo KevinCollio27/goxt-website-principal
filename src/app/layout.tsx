e4bbfce1b101835637ff670320b417d40292c9f9
KevinCollio27 <kevin.collio@goxt.io>
2026-05-29 13:17:06 -0400

    Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,6 +89,18 @@ export default function RootLayout({
           data-api-key="wk_0ac4fed868da91973c3916dfa0f4a7c4d3a9ead19b97772a"
           strategy="afterInteractive"
         />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`,
+          }}
+        />
       </body>
     </html>
   );
